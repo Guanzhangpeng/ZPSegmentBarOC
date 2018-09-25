27a0db72495aba8ee5d18d9339cbc6face16c894
gzp <zswangzp@163.com>
2018-09-25 13:16:34 +0800

    文字在下 图片在上样式

--- Example/ZPSegmentBarOC/ZPViewController.m
+++ Example/ZPSegmentBarOC/ZPViewController.m
@@ -26,7 +26,7 @@
     [self setAutomaticallyAdjustsScrollViewInsets:NO];
     
     CGRect frame=CGRectMake(0, 0, self.view.width, self.view.height);
-        NSArray * titles=@[@"头条",@"娱乐",@"视频"];
+        NSArray * titles=@[@"头条",@"娱乐",@"直播",@"视频"];
 //    NSArray * titles = @[@"推荐",@"热点",@"直播",@"视频",@"阳光视频",@"社会热点",@"娱乐",@"科技",@"汽车"];
     NSMutableArray * childVcs=[NSMutableArray array];
     for (int i=0; i<titles.count; i++) {
@@ -37,11 +37,19 @@
     
     ZPSegmentBarStyle * style=[[ZPSegmentBarStyle alloc] init];
     style.isScrollEnabled=NO;//导航条是否可以滚动,默认YES;
-    style.isShowCover=YES;//导航条是否显示遮盖效果,默认YES;
-    style.coverViewMargin=6;//遮盖间距;
-    style.isShowBottomLine=YES;//导航条下方是否显示BottomLine,默认YES;
-    style.bottomLineColor=[UIColor orangeColor];//BottomLine 的颜色;
-    style.isNeedScale=YES;//导航条是否有放大效果,默认YES;
+//    style.isShowCover=YES;//导航条是否显示遮盖效果,默认YES;
+//    style.coverViewMargin=6;//遮盖间距;
+//    style.isShowBottomLine=YES;//导航条下方是否显示BottomLine,默认YES;
+//    style.bottomLineColor=[UIColor orangeColor];//BottomLine 的颜色;
+//    style.isNeedScale=YES;//导航条是否有放大效果,默认YES;
+    
+    style.isShowImage = YES;
+    style.imageNames = @[@"message_sys",@"message_tz",@"message_sys",@"message_tz"];
+    style.selectedImageNames = @[@"message_sys_h",@"message_tz_h",@"message_sys_h",@"message_tz_h"];
+    style.imageSize = CGSizeMake(40.f, 40.f);
+    style.segmentBarHeight = 70.f;
+    style.isNeedScale = NO;
+    style.titleHeight = 18.f;
     self.style = style;
     
     ZPSegmentView * segmentView=[[ZPSegmentView alloc]initWithFrame:frame];
@@ -50,7 +58,7 @@
     [self.view addSubview:segmentView];
     
     //3秒后自动滚动到第二个item上;
-    [self performSelector:@selector(demo1) withObject:NULL afterDelay:3];
+//    [self performSelector:@selector(demo1) withObject:NULL afterDelay:3];
     
 }
 -(void)demo1
